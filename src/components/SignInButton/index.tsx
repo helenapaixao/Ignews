96d90386eb363457534940df33f2e239583586c1
helenapaixao <hp.helenapaixao@gmail.com>
2021-10-11 00:02:01 -0300

    feat: page home done

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,19 @@
-
-import { FaGithub } from 'react-icons/fa'
-import styles from './styles.module.scss'
-import {FiX} from 'react-icons/fi'
+import { FaGithub } from "react-icons/fa";
+import styles from "./styles.module.scss";
+import { FiX } from "react-icons/fi";
 
 export function SignInButton() {
   const isUserLoggedIn = true;
   return isUserLoggedIn ? (
- <button type="button"
-    className={styles.signInButton}
-    >
+    <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d301" />
       Helena Paixão
-      <FiX color="#737380" className={styles.closeIcon}/>
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
-  ): (
-       <button type="button"
-    className={styles.signInButton}
-    >
+  ) : (
+    <button type="button" className={styles.signInButton}>
       <FaGithub color="#eba417" />
-      Sign  in with GitHub
+      Sign in with GitHub
     </button>
-  )
+  );
 }
