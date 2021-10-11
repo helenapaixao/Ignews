a2137c490c1a2a813a0b99bb07be15aa5e14193c
helenapaixao <hp.helenapaixao@gmail.com>
2021-10-10 23:23:33 -0300

    feat: header almost done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
+import { AppProps } from "next/app";
+import "../../styles/global.scss";
+import { Header } from "../components/Header";
 
+function MyApp({ Component, pageProps }: AppProps) {
 
+  return (
+  <>
+    <Header />
+    <Component {...pageProps} />
+  </>
+  
+  )
+      
 
-import {AppProps} from 'next/app'
-import '../../styles/global.scss'
-
-
-function MyApp({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp;
